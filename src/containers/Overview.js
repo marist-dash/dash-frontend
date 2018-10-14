dc9499e2f04e8f966761b8d61d8ea675507caf22
Tyler <tgalske@gmail.com>
2018-10-13 23:59:45 -0400

    Added requirements tab in Overview

--- src/containers/Overview.js
+++ src/containers/Overview.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {FaRegCheckCircle, FaTimesCircle} from "react-icons/fa";
 
 class Overview extends Component {
 
@@ -17,19 +18,30 @@ class Overview extends Component {
           "Cybersecurity",
           "Information Technology",
           "Information Systems",
-        ]
+        ],
+        requirements: {
+          has20GPA: "COMPLETE",
+          hasBreadth: "COMPLETE",
+          hasFoundation: "COMPLETE",
+          hasHonors: "NA",
+          hasMajor: "INCOMPLETE",
+          hasMinCredits: "INCOMPLETE",
+          hasMinGPA: "COMPLETE",
+          hasMinLibArts: "COMPLETE",
+          hasPathway: "COMPLETE",
+          hasSkill: "COMPLETE",
+          isLast30Credits: "COMPLETE"
+        }
       }
     };
   };
 
   render() {
+    console.log(this.state.student);
     return (
       <div>
-        <h1 className="pl-4 pt-4 text-grey-darkest">Overview</h1>
-        <p className="pl-4 pt-2 text-grey-dark">View your degree progress</p>
-
-        <Header student={this.state.student}/>
-
+        <Header firstName={this.state.student.firstName} lastName={this.state.student.lastName} CWID={this.state.student.CWID}/>
+        <RequirementsTab requirements={this.state.student.requirements}/>
       </div>
     );
   }
@@ -37,10 +49,74 @@ class Overview extends Component {
 
 function Header(props) {
   return (
-    <div>
-      <p className="text-right">Lastname: {props.student.lastName}</p>
+    <div className="flex justify-between">
+
+      {/* Left side header */}
+      <div>
+        <h1 className="pl-8 pt-8 text-grey-darkest">Overview</h1>
+        <p className="pl-8 pt-2 text-grey-dark">View your degree progress</p>
+      </div>
+
+      {/* Right side header */}
+      <div className="text-lg">
+        {/* Name */}
+        <p className="pr-8 pt-8 text-right text-grey-darkest font-bold">
+          {props.firstName} {props.lastName}
+        </p>
+
+        {/* CWID */}
+        <div className="pr-8 pt-1 text-right text-grey-darkest tracking-wide">
+          {props.CWID}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
+function RequirementsTab(props) {
+  return (
+    <div className="max-w-sm m-8 pt-2 border-t-4 border-indigo-light">
+      <p className="text-center font-bold text-lg text-grey-darkest">Requirements</p>
+      <Requirement name={"Breadth"} status={props.requirements.hasBreadth}/>
+      <Requirement name={"Pathway"} status={props.requirements.hasPathway}/>
+      <Requirement name={"Foundation"} status={props.requirements.hasFoundation}/>
+      <Requirement name={"Major Requirements"} status={props.requirements.hasMajor}/>
+      <Requirement name={"Honors Program"} status={props.requirements.hasHonors}/>
+      <Requirement name={"Minimum Degree Credits"} status={props.requirements.hasMinCredits}/>
+      <Requirement name={"2.0 GPA Requirement"} status={props.requirements.has20GPA}/>
+      <Requirement name={"Minium GPA"} status={props.requirements.hasMinGPA}/>
+      <Requirement name={"Minium Liberal Arts Credits"} status={props.requirements.hasMinLibArts}/>
+      <Requirement name={"Skill Requirement"} status={props.requirements.hasSkill}/>
+      <Requirement name={"Last 30 Credits at Marist"} status={props.requirements.isLast30Credits}/>
+
+
+
+    </div>
+  );
+}
+
+function Requirement(props) {
+  if (props.status === "NA") {
+    return null;
+  } else {
+    return (
+      <div className="flex flex-row my-4 shadow-md rounded-lg bg-grey-lighter hover:bg-grey-light">
+        <div className="flex-shrink">
+          {props.status === "COMPLETE" ? (
+          <FaRegCheckCircle className="mt-2 ml-12 text-4xl text-green-dark opacity-75"/>
+          ) : (
+            <FaTimesCircle className="mt-2 ml-12 text-4xl text-red opacity-75"/>
+          )
+          }
+        </div>
+        <div className="flex-1">
+          <p className="my-4 -ml-8 text-center text-grey-darkest text-xl"> {props.name}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+
 
 export default Overview;
