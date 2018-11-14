487fcb401d6d83227cf9506fdda7f50f41c5fa3a
sgerbasi <stephen.gerbasi1@marist.edu>
2018-11-14 17:43:10 -0500

    Update Header.js
    added new speedegree logo

--- src/utils/Header.js
+++ src/utils/Header.js
@@ -29,7 +29,7 @@ class Header extends Component {
                     </div>
                   ) : (
                     <div className="fixed pin-t mt-1 pin-r mr-1 z-50 xl:relative xl:flex xl:justify-start xl:justify-end xl:text-right xl:justify-end xl:mt-8 xl:mr-24">
-                      <InitialsCircle/>
+                      <InitialsCircle/> 
                     </div>
                   )
                 }
